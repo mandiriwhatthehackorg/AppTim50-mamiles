80dd635493bc484c660d99ad41e660ad064acd16
junaedi.widjojo <junaedi.widjojo@gmail.com>
2019-07-21 01:57:54 +0700

    Splash

--- app/src/main/java/com/hackathon/mamiles/SplashActiveActivity.java
+++ app/src/main/java/com/hackathon/mamiles/SplashActiveActivity.java
@@ -11,6 +11,13 @@ public class SplashActiveActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         Intent intent = new Intent(this, ActiveActivity.class);
         startActivity(intent);
         finish();
--- app/src/main/java/com/hackathon/mamiles/SplashActivity.java
+++ app/src/main/java/com/hackathon/mamiles/SplashActivity.java
@@ -11,6 +11,12 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
